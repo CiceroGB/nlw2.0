c51e127fc7a8da7b1aceb8750ebe15cfdcfe3259
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-03 19:23:35 -0300

    add styles on div teachers

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 
@@ -24,6 +26,33 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars1.githubusercontent.com/u/63745509?s=460&u=e109864234f83424253a3ebe205835e0db7894e7&v=4" alt="Cicero"/>
+                    <div>
+                        <strong>Cicero</strong>
+                        <span>Química</span>
+                    </div>
+                    </header>
+                    <p>
+                        textão 
+                        <br/><br/>
+                        textão
+                    </p>
+                    <footer>
+                        <p>
+                            Preço/Hora
+                            <strong>R$80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="WhatsApp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }
