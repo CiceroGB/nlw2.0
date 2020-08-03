5597ac1e58e4f77e08f74ba4da9a71b3414971fc
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-03 16:16:08 -0300

    add paging landing

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
-
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 // import { Container } from './styles';
 
@@ -21,7 +23,21 @@ function Landing() {
                     className="hero-image"
                 />
 
-                <div className="buttons-container"></div>
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar" />
+                        Estudar
+                    </a>
+                    <a href="" className="study">
+                        <img src={giveClassesIcon} alt="give-classes" />
+                        Estudar
+                     </a>
+                </div>
+                <span className="total-conections">
+                    Total de 200 conexões realizadas
+                    <img src={purpleHeartIcon} alt="Coração Roxo" />
+                </span>
+
             </div>
 
         </div>
