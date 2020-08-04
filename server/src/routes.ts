a8f1b9752a33ea89a43c8be8bec3d220aa49cdfb
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-04 13:09:05 -0300

    try cath in post classes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,37 +1,69 @@
 import express from 'express';
 import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes = express.Router();
+
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
 routes.post('/classes', async (req, res) => {
     const {
         name,
-        avatar, 
-        whatsapp, 
-        bio, 
-        subject, 
-        cost, 
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
         schedule
     } = req.body;
 
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio
-    })
+    const trx = await db.transaction();
 
-   const user_id = insertedUsersIds[0];
+    try {
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio
+        })
 
-    await db('classes').insert({
-        subject,
-        cost,
-        user_id,
+        const user_id = insertedUsersIds[0];
 
-    })
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+
+        })
+
+        const class_id = insertedClassesIds[0];
+
+        const classScheduele = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to)
+            };
+        })
+
+        await trx('class_schedule').insert(classScheduele);
+
+        await trx.commit();
 
 
-    res.json();
+        return res.status(201).send();
+
+    } catch (err) {
+        await trx.rollback();
+        return res.status(400).json({
+            error: 'Error while creating new class'
+        })
+    }
 
 
 });
