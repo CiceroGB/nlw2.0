6ce9e9fe9c75fa64b9911494c94232dda299f0f3
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-04 14:46:18 -0300

    get classController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 const classesController = new ClassesController();
 
-
+routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
 
