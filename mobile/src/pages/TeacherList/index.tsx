383bc5178c683a33bf2ba92481fd47d4a146c368
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-08 17:12:15 -0300

    add filter to search proffys

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,20 +1,77 @@
-import React from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, Text, TextInput } from 'react-native';
 import styles from './styles';
 import PageHeader from '../../Components/PageHeader';
 import TeacherItem from '../../Components/TeacherItem';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 function TeacherList() {
+
+    const [isFiltersVisible, setIsFilterVisible] = useState(false);
+
+    function handleFilter() {
+        setIsFilterVisible(!isFiltersVisible);
+    }
+
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis" />
+            <PageHeader
+                title="Proffys disponíveis"
+                headerRight={
+                    <BorderlessButton onPress={handleFilter}>
+                        <Feather
+                            name="filter"
+                            size={20}
+                            color="#FFF"
+                        />
+                    </BorderlessButton>
+                }
+            >
+
+                {isFiltersVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Matéria</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Matéria"
+                            placeholderTextColor="#c1bccc"
+                        />
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Dia da Semana</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Qual dia?"
+                                    placeholderTextColor="#c1bccc"
+                                />
+                            </View>
+
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Horário</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Qual hora?"
+                                    placeholderTextColor="#c1bccc"
+                                />
+                            </View>
+
+                        </View>
+
+                        <RectButton style={styles.submitButton}>
+                            <Text style={styles.submitButtonText}>Filtar</Text>
+
+                        </RectButton>
+                    </View>
+                )}
+            </PageHeader>
 
             <ScrollView
-            style={styles.teacherList}
-            contentContainerStyle= {{
-                paddingHorizontal:16,
-                paddingBottom:16
-            }}            
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16
+                }}
             >
                 <TeacherItem />
                 <TeacherItem />
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -2,14 +2,58 @@ import { StyleSheet } from 'react-native';
 
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        backgroundColor:'#F0F0F7'
+        backgroundColor: '#F0F0F7'
     },
-    teacherList:{
+    teacherList: {
         marginTop: -40,
 
     },
+    searchForm: {
+        marginBottom: 24,
+    },
+
+    label: {
+        color: '#dac2ff',
+        fontFamily: 'Poppins_400Regular',
+    },
+
+    inputGroup: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    inputBlock: {
+        width: '48%',
+    },
+
+    input: {
+        height: 54,
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        marginTop: 4,
+        marginBottom: 16,
+    },
+
+    submitButton: {
+        backgroundColor: '#04d361',
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    submitButtonText: {
+        color: '#fff',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16,
+    },
+
+
 });
 
 export default styles;
