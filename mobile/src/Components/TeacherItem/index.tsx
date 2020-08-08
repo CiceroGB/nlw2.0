381d07e11425039625fcac2a5bb3304b340d311c
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-08 16:06:15 -0300

    teacher item and style

---
+++ mobile/src/Components/TeacherItem/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutLineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+
+import styles from './styles';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://avatars1.githubusercontent.com/u/63745509?s=460&u=e109864234f83424253a3ebe205835e0db7894e7&v=4' }}
+        />
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>
+            Nomes
+            </Text>
+          <Text style={styles.subject}>
+            Matérias
+            </Text>
+        </View>
+      </View>
+      <Text style={styles.bio}>
+        Bio bio
+      </Text>
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/Hora {'  '}
+          <Text style={styles.priceValue}>
+            R$ 50,00
+          </Text>
+        </Text>
+        <View style={styles.buttonContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutLineIcon} />
+          </RectButton>
+
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>
+              Entrar em contato
+            </Text>
+          </RectButton>
+
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default TeacherItem;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import styles from './styles';
 import PageHeader from '../../Components/PageHeader';
+import TeacherItem from '../../Components/TeacherItem';
 
 function TeacherList() {
     return (
         <View style={styles.container}>
             <PageHeader title="Proffys disponíveis" />
+            <TeacherItem />
         </View>
     )
 }
