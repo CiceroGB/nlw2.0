360df1989921515b126a6297288c3527c518a23f
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-09 02:27:12 -0300

    favorite navigation

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { View ,ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { View ,ScrollView, AsyncStorage} from 'react-native';
+import {useFocusEffect} from '@react-navigation/native';
 
 import styles from './styles';
 import PageHeader from '../../Components/PageHeader';
-import TeacherItem from '../../Components/TeacherItem';
+import TeacherItem, { Teacher } from '../../Components/TeacherItem';
 
 function Favorites() {
+    const [favorites, setFavorites] = useState([]);
+
+    function loadFavorites(){
+        AsyncStorage.getItem('favorites').then(res => {
+            if (res) {
+                const favoreted = JSON.parse(res);
+                setFavorites(favoreted)
+            }
+        })
+    }
+    useFocusEffect(()=>{loadFavorites()});
+
     return (
 
         <View style={styles.container}>
@@ -17,12 +30,17 @@ function Favorites() {
                     paddingBottom: 16
                 }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+
+                {favorites.map((teacher: Teacher)=>{
+                    return (
+                        <TeacherItem 
+                        key={teacher.id}
+                        teacher= {teacher}
+                        favorited = {true}
+                        />
+                    )
+                })}
+
             </ScrollView>
         </View>
 
